2664d220899c52c54c2f5d2d4b9e7fb046389135
Luciaalonso1988 <103374818+Luciaalonso1988@users.noreply.github.com>
2022-10-19 21:31:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath));
-const port = 3000;
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
 app.listen(port, () => {
-    console.log("servidor funcionando")
+    console.log(`Servidor correndo en puerto ${port}`);
 })
